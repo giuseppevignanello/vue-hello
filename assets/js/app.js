2551fb15d5a50c4ed0a911dd68c5f85a5cb97079
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-28 14:55:52 +0200

    add img

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,8 @@
   createApp({
     data() {
       return {
-        title: 'Hello Vue!'
+        title: 'Hello Vue!', 
+        image: 'https://picsum.photos/200/300'
       }
     }
   }).mount('#app')
